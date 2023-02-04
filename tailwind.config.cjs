17bb1385528ab0492563defb4a27f5b739321efe
Skumbl <111002282+Skumbl@users.noreply.github.com>
2023-02-04 14:48:56 -0500

    basic start

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -2,7 +2,13 @@
 module.exports = {
   content: ["./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+        fontFamily: {
+            LibreBask: "LibreBaskerville",
+            LibreBaskBold: "LibreBaskerville-Bold",
+            LibreBaskItalic: "LibreBaskerville-Italic"
+        }
+    },
   },
   plugins: [],
 };
