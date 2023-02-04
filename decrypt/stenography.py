b3351d9de2f2ae2439cb202bb14ee936ba7d9c69
Truman DeWalch <trumandewalch@gmail.com>
2023-02-04 17:08:30 -0500

    sync

--- decrypt/stenography.py
+++ decrypt/stenography.py
@@ -8,24 +8,46 @@ from itertools import combinations
 
 def encoding(imagePath, dataToEncrypt):
     imageData   = cv2.imread(imagePath)
-    numBitsPic  = int(np.floor(imageData.shape[0] * imageData.shape[1] * 3/8))
+    numBitsPic  = int(np.floor(imageData.shape[0] * imageData.shape[1] * 3 * 8))
+    numBytesPic = int(np.floor(imageData.shape[0] * imageData.shape[1] * 3))
     binaryData  = ''.join(format(ord(i), '08b') for i in dataToEncrypt) + '00000000'
     numBitsData = len(binaryData)
 
-    curDataIdx  = 0
-    for i,row in enumerate(imageData):
-        # print('Encoding row: ', i, ' of ', imageData.shape[0])
-        for j,pixel in enumerate(row):
-            # convert RGB values to binary format
-            for k,rgb in enumerate(pixel):
-                if curDataIdx < len(binaryData):
-                    rgb         = format(rgb, '08b')
-                    rgb         = rgb[:-1] + str(binaryData[curDataIdx])
-                    curDataIdx += 1
-                    # convert rgb back to int
-                    imageData[i][j][k] = int(rgb, 2)
+    numLocations     = 10
+    maxNumBits       = int(np.ceil(np.log2(numBytesPic)))
+    numBytesReserved = numLocations * (8 + maxNumBits)
+    numBytesUsable   = numBytesPic-numBytesReserved
+    indices = np.random.randint(0, numBytesUsable, numLocations)
+    indices = np.sort(indices)
 
-    cv2.imwrite('decrypt/stockimageEncrypted.png', imageData)
+    maxNum  = len(format(numBytesPic, 'b'))
+    for i,num in enumerate(indices):
+        num = format(num, 'b')
+        num = num.zfill(maxNum)
+
+    # curDataIdx = 0
+    # for i,row in enumerate(imageData):
+    #     for j,pixel in enumerate(row):
+    #         # convert RGB values to binary format
+    #         for k,rgb in enumerate(pixel):
+    #             if curDataIdx < len(binaryData):
+    #                 rgb         = format(rgb, '08b')
+    #                 rgb         = rgb[:-1] + str(binaryData[curDataIdx])
+    #                 curDataIdx += 1
+    #                 # convert rgb back to int
+    #                 imageData[i][j][k] = int(rgb, 2)
+    
+    curRGBIdx  = 0
+    imageData = imageData[::-1, ::-1, ::-1]
+    print('imageData: ', imageData)
+    # for i,row in enumerate(imageData):
+    #     for j,pixel in enumerate(row):
+    #         # convert RGB values to binary format
+    #         for k,rgb in enumerate(pixel):
+
+    #             curRGBIdx += 1
+                
+    # cv2.imwrite('decrypt/stockimageEncrypted.png', imageData)
     return
 
 
@@ -103,4 +125,4 @@ def invert(h):
 
 if __name__ == '__main__':
     encoding('decrypt/stockimage.png', 'hello world')
-    print(decode('decrypt/stockimageEncrypted.png'))
+    # print(decode('decrypt/stockimageEncrypted.png'))
