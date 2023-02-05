b916d3cca1c0c647b8e235a667741547fbd8f87b
Truman DeWalch <trumandewalch@gmail.com>
2023-02-05 04:01:53 -0500

    testing

--- testImporting.py
+++ testImporting.py
@@ -1,10 +1,10 @@
 from hips_hack.stenography import encode, decode
 
-encode(imagePath='/Users/trumandewalch/Hackathon/decrypt/stockimage.png', dataToEncrypt='hello!', outputImagePath='/Users/trumandewalch/Hackathon/decrypt/stockimage_encrypted.png')
-message = decode(imagePath='/Users/trumandewalch/Hackathon/decrypt/stockimage_encrypted.png')
+# encode(imagePath='/Users/trumandewalch/Downloads/IMG_0632.JPG', dataToEncrypt='hello!', outputImagePath='/Users/trumandewalch/Downloads/IMG_0632_encrypted.JPG')
+message = decode(imagePath='/Users/trumandewalch/Downloads/IMG_0632_encrypted.png')
 print(message)
 
 
-encode(imagePath='decrypt/stockimage.png', dataToEncrypt='hello!', outputImagePath='decrypt/stockimage_encrypted.png')
-message = decode(imagePath='decrypt/stockimage_encrypted.png')
-print(message)
+# encode(imagePath='decrypt/stockimage.png', dataToEncrypt='hello!', outputImagePath='decrypt/stockimage_encrypted.png')
+# message = decode(imagePath='decrypt/stockimage_encrypted.png')
+# print(message)
